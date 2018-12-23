7cac278dca876658418109e5b645f40de5693c73
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-12-23 17:19:23 +0000

    :art: Refactor example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ImageSlider from './lib';
+import Slider from './lib';
+
+const imageData = [
+  {
+    src: '/assets/slide1.jpg',
+    title: 'Slide 1'
+  },
+  {
+    src: '/assets/slide2.jpg',
+    title: 'Slide 2'
+  },
+  {
+    src: '/assets/slide3.jpg',
+    title: 'Slide 3'
+  }
+];
 
 const App = () => (
-  <>
+  <React.Fragment>
     <div
       style={{
         position: 'absolute',
@@ -16,13 +31,9 @@ const App = () => (
       <h1>AC Simple React Slider</h1>
     </div>
     <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
-      <ImageSlider>
-        <img key='1' src='/assets/slide1.jpg' alt='Slide 1' />
-        <img key='2' src='/assets/slide2.jpg' alt='Slide 2' />
-        <img key='3' src='/assets/slide3.jpg' alt='Slide 3' />
-      </ImageSlider>
+      <Slider data={imageData} />
     </div>
-  </>
+  </React.Fragment>
 );
 
 render(<App />, document.getElementById('root'));
