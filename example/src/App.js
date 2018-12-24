6aadaf164ff3c6b659bbb55db335ee767c0baf70
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-12-24 22:49:40 +0000

    :sparkles: Add example

---
+++ example/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Slider from 'ac-react-simple-image-slider-test';
+
+const imageData = [
+  {
+    src: '/assets/slide1.jpg',
+    title: 'Slide 1'
+  },
+  {
+    src: '/assets/slide2.jpg',
+    title: 'Slide 2'
+  },
+  {
+    src: '/assets/slide3.jpg',
+    title: 'Slide 3'
+  }
+];
+
+const App = () => (
+  <React.Fragment>
+    <div
+      style={{
+        position: 'absolute',
+        zIndex: '2',
+        color: '#fff',
+        textAlign: 'center',
+        width: '100%'
+      }}
+    >
+      <h1>AC Simple React Slider</h1>
+    </div>
+    <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <Slider data={imageData} />
+    </div>
+  </React.Fragment>
+);
+
+export default App;
