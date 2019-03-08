307aad328a90ee69569892fe090347d20b9b836c
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-03-08 15:22:03 +0000

    :zap: Replace styled-components with emotion

--- src/lib/Slider.styles.js
+++ src/lib/Slider.styles.js
@@ -1,4 +1,5 @@
-import styled, { css, keyframes } from 'styled-components';
+import styled from '@emotion/styled';
+import { css, keyframes } from '@emotion/core';
 import { ifProp, prop } from 'styled-tools';
 
 export const SliderWrapper = styled.div`
